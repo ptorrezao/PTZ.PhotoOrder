266fd57e187fa5f6c336390fc91c86c376b3624b
Pedro Torrezão <pedro.torrezao@gmail.com>
2019-12-28 17:34:28 +0000

    Add project files.

---
+++ PTZ.PhotoOrder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PTZ.PhotoOrder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
